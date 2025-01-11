d9e0e612ecf5bb0ef3f1662881d22596a7deccc2
esther <estherpeula2@gmail.com>
2025-01-11 18:18:50 +0100

    Actualizar comentarios

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import random
 
 segments = []
 
+
+# Funciones para mover la serpiente con las teclas
 def up():
     if new_segment.heading() != 270:
         new_segment.setheading(90)
@@ -18,15 +20,15 @@ def right():
     if new_segment.heading() != 180:
         new_segment.setheading(0)
 
-# Pantalla
+# Pantalla // Configuración pantalla de juego
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-screen.tracer(0) #Interrumpe la animación
+screen.tracer(0) # Interrumpe la animación (evitar giro del segmento)
 
 
-#Serpiente
+# Serpiente // Configuración serpiente
 new_segment = turtle.Turtle()
 new_segment.shape("square")
 new_segment.color("white")
@@ -34,7 +36,7 @@ new_segment.penup()
 new_segment.goto(0,0)
 segments.append(new_segment)
 
-#Comida // 
+# Comida // Configuración comida
 food = turtle.Turtle("circle")
 food.color("red")
 food.penup()
@@ -43,7 +45,7 @@ y = random.randint(-280, 280)
 food.goto(x, y) #Posiciona la comida en una posición aleatoria
 
 
-# Score // 
+# Score // Configuración marcador de puntuación
 score = turtle.Turtle()
 score.color("white")
 score.penup()
@@ -52,31 +54,31 @@ score.hideturtle()
 score_board = 0
 score.write(f"Score: {score_board}", align="center", font=("Arial", 24, "bold"))
 
-
+# Movimiento de la serpiente
 screen.onkey(up, "Up")
 screen.onkey(down, "Down")
 screen.onkey(left, "Left")
 screen.onkey(right, "Right")
+screen.listen() # Escucha las teclas
 
-screen.listen()
-
+# Bucle de juego
 game_is_on = True
 while game_is_on:
     screen.update() #Actualiza la animación
     time.sleep(0.3)
 
-# Detectar toque comida
+    # Detectar toque comida // Aumentar tamaño de la serpiente y mover comida a posición aleatoria
     if new_segment.distance(food) < 15:
         x = random.randint(-280, 280)
         y = random.randint(-280, 280)
-        food.goto(x, y) #Mueve la comida a una posición aleatoria
+        food.goto(x, y) 
 
         next_segment = turtle.Turtle("square")
         next_segment.color("white")
         next_segment.penup()
         segments.append(next_segment)
 
-        #Actualizar puntuación
+        # Actualizar puntuación
         score.clear()
         score_board += 1
         score.write(f"Score: {score_board}", align="center", font=("Arial", 24, "bold"))
